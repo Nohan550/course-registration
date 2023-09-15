44e79005fdf33396560e72a7c15294fdbbe003b8
Nohan550 <nohanmuttakim.bd@gmail.com>
2023-09-15 23:32:45 +0600

    header added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+   
+      <h1 className='text-3xl font-bold text-center py-12'>Course Registration</h1>
+      
+    </div>
+  )
+}
+
+export default App
